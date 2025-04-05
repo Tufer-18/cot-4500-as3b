5f8016e75f589540f7c0d6b29964d791e6fd0d5f
Tufer-18 <pr557116@ucf.edu>
2025-04-04 22:01:53 -0400

    Update test_ assignment_3.py

--- test/test_ assignment_3.py
+++ test/test_ assignment_3.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def gaussian_elimination(A, b):
+def gaussian_elimination(A, b): #Gaussian Elimination Function
     A = A.astype(float)
     b = b.astype(float)
     n = len(A)
@@ -23,7 +23,7 @@ def gaussian_elimination(A, b):
     return x
 
 
-def lu_factorization(A):
+def lu_factorization(A): #LU Factorization Function
     n = len(A)
     L = np.eye(n)
     U = A.astype(float)
@@ -37,7 +37,7 @@ def lu_factorization(A):
     determinant = np.prod(np.diag(U))
     return L, U, determinant
 
-def is_diagonally_dominant(A):
+def is_diagonally_dominant(A): #Program to check for diagonal dominance
     for i in range(len(A)):
         row_sum = sum(abs(A[i, j]) for j in range(len(A)) if i != j)
         if abs(A[i, i]) < row_sum:
@@ -46,7 +46,7 @@ def is_diagonally_dominant(A):
     
     return True
 
-def is_positive_definite(A):
+def is_positive_definite(A): #Function to check for positive definite.
     try:
         np.linalg.cholesky(A)
        
@@ -56,10 +56,12 @@ def is_positive_definite(A):
         return False
 
 
+# Gaussian Elimination function test------------------------------------------------------------------------------------------------------------------------------
 A = np.array([[2, -1, 1], [1, 3, 1], [-1, 5, 4]], dtype=float)
 b = np.array([6, 0, -3], dtype=float)
 print(gaussian_elimination(A, b))
 
+# LU Factorization Function test----------------------------------------------------------------------------------------------------------------------------------
 A = np.array([[1, 1, 0, 3], [2, 1, -1, 1], [3, -1, -1, 2], [-1, 2, 3, -1]], dtype=float)
 L, U, det_A = lu_factorization(A)
 print("\n", det_A)
@@ -67,6 +69,7 @@ print("\n", L)
 print("\n", U)
 print("\n")    
 
+# Diagonal Dominant test---------------------------------------------------------------------------------------------------------------------------------------------
 A = np.array([
     [9, 0, 5, 2, 1],
     [3, 9, 1, 2, 1],
@@ -76,6 +79,9 @@ A = np.array([
     ], dtype=float)
 print("Diagonally Dominant:", is_diagonally_dominant(A))
 print("\n")
+
+
+# Positive Definite test-----------------------------------------------------------------------------------------------------------------------------------------------
 A = np.array([
      [2, 2, 1],
      [2, 3, 0],
